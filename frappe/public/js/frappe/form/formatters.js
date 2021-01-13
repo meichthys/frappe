meichthys/frappe

commit 97b693c6b0c31f7ae652d2faa88648440fd5f81a

feat: Added permission to grant only `Select` access to document (#12063)
* feat: add permtype 'select' to DocPerm and CustomDocPerm

* feat: add 'select' perm in rights tupple

* feat: provisions to handle select permission

* feat: toggle href based on permissions

* feat: pass permission type explicitly while validating link in permission check

* fix: sider

* feat: added test cases to validate select perm

* feat: add method frappe.only_has_select_perm to explicitly check the select perm

* fix: if user only has select perm then do not show anchor tag for link fields

* fix: sider

frappe/public/js/frappe/form/formatters.js
@@ -128,11 +128,16 @@ frappe.form.formatters = {
 			return repl('<a onclick="%(onclick)s">%(value)s</a>',
 				{onclick: docfield.link_onclick.replace(/"/g, '&quot;'), value:value});
 		} else if(docfield && doctype) {
-			return `<a class="grey"
-				href="#Form/${encodeURIComponent(doctype)}/${encodeURIComponent(original_value)}"
-				data-doctype="${doctype}"
-				data-name="${original_value}">
-				${__(options && options.label || value)}</a>`
+			if (!frappe.model.can_select(doctype) && frappe.model.can_read(doctype)) {
+				return `<a class="grey"
+					#Form/${encodeURIComponent(doctype)}/${encodeURIComponent(original_value)}
+					data-doctype="${doctype}"
+					data-name="${original_value}">
+					${__(options && options.label || value)}</a>`;
+			} else {
+				return value;
+			}
+
 		} else {
 			return value;
 		}

frappe/__init__.py
@@ -628,6 +628,21 @@ def clear_cache(user=None, doctype=None):
 
 	local.role_permissions = {}
 
+def only_has_select_perm(doctype, user=None, ignore_permissions=False):
+	if ignore_permissions:
+		return False
+
+	if not user:
+		user = local.session.user
+
+	import frappe.permissions
+	permissions = frappe.permissions.get_role_permissions(doctype, user=user)
+
+	if permissions.get('select') and not permissions.get('read'):
+		return True
+	else:
+		return False
+
 def has_permission(doctype=None, ptype="read", doc=None, user=None, verbose=False, throw=False):
 	"""Raises `frappe.PermissionError` if not permitted.
 

frappe/core/page/permission_manager/permission_manager.js
@@ -269,7 +269,7 @@ frappe.PermissionEngine = Class.extend({
 			.css({"margin-top": "15px"});
 	},
 
-	rights: ["read", "write", "create", "delete", "submit", "cancel", "amend",
+	rights: ["select", "read", "write", "create", "delete", "submit", "cancel", "amend",
 		"print", "email", "report", "import", "export", "set_user_permissions", "share"],
 
 	set_show_users: function(cell, role) {

frappe/desk/search.py
@@ -150,7 +150,8 @@ def search_widget(doctype, txt, query=None, searchfield=None, start=0,
 			# 2 is the index of _relevance column
 			order_by = "_relevance, {0}, `tab{1}`.idx desc".format(order_by_based_on_meta, doctype)
 
-			ignore_permissions = True if doctype == "DocType" else (cint(ignore_user_permissions) and has_permission(doctype))
+			ptype = 'select' if frappe.only_has_select_perm(doctype) else 'read'
+			ignore_permissions = True if doctype == "DocType" else (cint(ignore_user_permissions) and has_permission(doctype, ptype=ptype))
 
 			if doctype in UNTRANSLATED_DOCTYPES:
 				page_length = None

frappe/model/db_query.py
@@ -40,7 +40,10 @@ class DatabaseQuery(object):
 		ignore_ifnull=False, save_user_settings=False, save_user_settings_fields=False,
 		update=None, add_total_row=None, user_settings=None, reference_doctype=None,
 		return_query=False, strict=True, pluck=None, ignore_ddl=False):
-		if not ignore_permissions and not frappe.has_permission(self.doctype, "read", user=user):
+		if not ignore_permissions and \
+			not frappe.has_permission(self.doctype, "select", user=user) and \
+			not frappe.has_permission(self.doctype, "read", user=user):
+
 			frappe.flags.error_message = _('Insufficient Permission for {0}').format(frappe.bold(self.doctype))
 			raise frappe.PermissionError(self.doctype)
 
@@ -315,7 +318,10 @@ class DatabaseQuery(object):
 	def append_table(self, table_name):
 		self.tables.append(table_name)
 		doctype = table_name[4:-1]
-		if (not self.flags.ignore_permissions) and (not frappe.has_permission(doctype)):
+		ptype = 'select' if frappe.only_has_select_perm(doctype) else 'read'
+
+		if (not self.flags.ignore_permissions) and\
+			 (not frappe.has_permission(doctype, ptype=ptype)):
 			frappe.flags.error_message = _('Insufficient Permission for {0}').format(frappe.bold(doctype))
 			raise frappe.PermissionError(doctype)
 
@@ -576,7 +582,7 @@ class DatabaseQuery(object):
 		self.shared = frappe.share.get_shared(self.doctype, self.user)
 
 		if (not meta.istable and
-			not role_permissions.get("read") and
+			not (role_permissions.get("select") or role_permissions.get("read")) and
 			not self.flags.ignore_permissions and
 			not has_any_user_permission_for_doctype(self.doctype, self.user, self.reference_doctype)):
 			only_if_shared = True

frappe/permissions.py
@@ -7,7 +7,7 @@ import frappe, copy, json
 from frappe import _, msgprint
 from frappe.utils import cint
 import frappe.share
-rights = ("read", "write", "create", "delete", "submit", "cancel", "amend",
+rights = ("select", "read", "write", "create", "delete", "submit", "cancel", "amend",
 	"print", "email", "report", "import", "export", "set_user_permissions", "share")
 
 # TODO:
@@ -73,6 +73,7 @@ def has_permission(doctype, ptype="read", doc=None, verbose=False, user=None, ra
 
 		role_permissions = get_role_permissions(meta, user=user)
 		perm = role_permissions.get(ptype)
+
 		if not perm:
 			push_perm_check_log(_('User {0} does not have doctype access via role permission for document {1}').format(frappe.bold(user), frappe.bold(doctype)))
 
@@ -192,9 +193,9 @@ def get_role_permissions(doctype_meta, user=None):
 				and ptype != 'create'):
 				perms['if_owner'][ptype] = 1
 				# has no access if not owner
-				# only provide read access so that user is able to at-least access list
+				# only provide select or read access so that user is able to at-least access list
 				# (and the documents will be filtered based on owner sin further checks)
-				perms[ptype] = 1 if ptype == 'read' else 0
+				perms[ptype] = 1 if ptype in ['select', 'read'] else 0
 
 		frappe.local.role_permissions[cache_key] = perms
 

frappe/public/js/frappe/form/controls/link.js
@@ -215,6 +215,7 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 					}
 					me.$input.cache[doctype][term] = r.results;
 					me.awesomplete.list = me.$input.cache[doctype][term];
+					me.toggle_href(doctype);
 				}
 			});
 		}, 500));
@@ -294,6 +295,15 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 			return [...newArr, currElem];
 		}, []);
 		// returns [{value: 'Manufacturer 1', 'description': 'mobile part 1, mobile part 2'}]
+	},
+
+	toggle_href(doctype) {
+		if (frappe.model.can_select(doctype) && !frappe.model.can_read(doctype)) {
+			// remove href from link field as user has only select perm
+			this.$input_area.find(".link-btn").addClass('hide');
+		} else {
+			this.$input_area.find(".link-btn").removeClass('hide');
+		}
 	},
 
 	get_filter_description(filters) {

frappe/public/js/frappe/model/model.js
@@ -135,8 +135,8 @@ $.extend(frappe.model, {
 			let cached_timestamp = null;
 			let cached_doc = null;
 
-			let cached_docs = frappe.model.get_from_localstorage(doctype)
-			
+			let cached_docs = frappe.model.get_from_localstorage(doctype);
+
 			if (cached_docs) {
 				cached_doc = cached_docs.filter(doc => doc.name === doctype)[0];
 				if(cached_doc) {
@@ -250,6 +250,10 @@ $.extend(frappe.model, {
 
 	can_create: function(doctype) {
 		return frappe.boot.user.can_create.indexOf(doctype)!==-1;
+	},
+
+	can_select: function(doctype) {
+		return frappe.boot.user.can_select.indexOf(doctype)!==-1;
 	},
 
 	can_read: function(doctype) {

commit 0db0b31d3e9eed2096ceb41c57f0bead8713ab5a

fix(desk): Correctly format and render Link fields
This seems to be broken after
https://github.com/frappe/frappe/pull/12063

https://github.com/saurabh6790/frappe/commit/00cc7df05e53f682a64a53b1479dd3140164a202

frappe/public/js/frappe/form/formatters.js
@@ -130,7 +130,7 @@ frappe.form.formatters = {
 		} else if(docfield && doctype) {
 			if (!frappe.model.can_select(doctype) && frappe.model.can_read(doctype)) {
 				return `<a class="grey"
-					#Form/${encodeURIComponent(doctype)}/${encodeURIComponent(original_value)}
+					href="#Form/${encodeURIComponent(doctype)}/${encodeURIComponent(original_value)}"
 					data-doctype="${doctype}"
 					data-name="${original_value}">
 					${__(options && options.label || value)}</a>`;