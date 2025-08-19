439e7e1df4bbb43a01bbcbe300b7b4247cec82cd
octex <octavio.gurnik@gmail.com>
2025-08-19 12:18:05 -0300

    fix: Fixed issue with translated options for dynamic link in list view filter

--- frappe/public/js/frappe/form/controls/dynamic_link.js
+++ frappe/public/js/frappe/form/controls/dynamic_link.js
@@ -7,17 +7,12 @@ frappe.ui.form.ControlDynamicLink = class ControlDynamicLink extends frappe.ui.f
 			//for dialog box
 			options = cur_dialog.get_value(this.df.options);
 		} else if (!cur_frm) {
-			const selector = `input[data-fieldname="${this.df.options}"]`;
-			let input = null;
 			if (cur_list) {
 				// for list page
-				input = cur_list.filter_area.standard_filters_wrapper.find(selector);
+				options = cur_list.page.fields_dict[this.df.options].get_input_value();
 			}
 			if (cur_page) {
-				input = $(cur_page.page).find(selector);
-			}
-			if (input) {
-				options = input.val();
+				options = cur_page.page.fields_dict[this.df.options].get_input_value();
 			}
 		} else {
 			options = frappe.model.get_value(this.df.parent, this.docname, this.df.options);
