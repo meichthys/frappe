abf2109559a571b6e56706099206c71bd032adbe
Rohit Waghchaure <rohitw1991@gmail.com>
2025-08-20 15:40:07 +0530

    fix: only consider the Frappe and ERPNext apps for setup completion checks
    (cherry picked from commit d5e1bf06b2b42cea72ee827582831e79edefee66)
    
    # Conflicts:
    #	frappe/__init__.py

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -1511,8 +1511,19 @@ def is_setup_complete():
 	if not frappe.db.table_exists("Installed Application"):
 		return setup_complete
 
+<<<<<<< HEAD
 	if all(frappe.get_all("Installed Application", {"has_setup_wizard": 1}, pluck="is_setup_complete")):
 		setup_complete = True
+=======
+	if all(
+		frappe.get_all(
+			"Installed Application",
+			{"app_name": ("in", ["frappe", "erpnext"])},
+			pluck="is_setup_complete",
+		)
+	):
+		is_setup_complete = True
+>>>>>>> d5e1bf06b2 (fix: only consider the Frappe and ERPNext apps for setup completion checks)
 
 	return setup_complete
 
