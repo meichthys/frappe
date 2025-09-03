a65a09b15e9da987369fc2d1d4679037cd0dbeb5
Rahul Agrawal <12agrawalrahul@gmail.com>
2025-09-03 11:28:46 +0530

    refactor: extract expression into separate variable

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -1036,7 +1036,8 @@ def sign_up(email: str, full_name: str, redirect_to: str) -> tuple[int, str]:
 			return 0, _("Registered but disabled")
 	else:
 		max_signups_allowed_per_hour = cint(frappe.get_system_settings("max_signups_allowed_per_hour") or 300)
-		if frappe.db.get_creation_count("User", 60) > max_signups_allowed_per_hour:
+		users_created_past_hour = frappe.db.get_creation_count("User", 60)
+		if users_created_past_hour > max_signups_allowed_per_hour:
 			frappe.respond_as_web_page(
 				_("Temporarily Disabled"),
 				_(
