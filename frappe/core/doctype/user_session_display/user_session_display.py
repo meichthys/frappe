ec6dbb6a040247ad948f5ac96da5e1d8e46e25e9
barredterra <14891507+barredterra@users.noreply.github.com>
2025-09-10 11:46:25 +0200

    chore(User Session Display): remove old boilerplate

--- frappe/core/doctype/user_session_display/user_session_display.py
+++ frappe/core/doctype/user_session_display/user_session_display.py
@@ -24,27 +24,3 @@ class UserSessionDisplay(Document):
 		session_created: DF.Datetime | None
 		user_agent: DF.SmallText | None
 	# end: auto-generated types
-
-	def db_insert(self, *args, **kwargs):
-		raise NotImplementedError
-
-	def load_from_db(self):
-		raise NotImplementedError
-
-	def db_update(self):
-		raise NotImplementedError
-
-	def delete(self):
-		raise NotImplementedError
-
-	@staticmethod
-	def get_list(filters=None, page_length=20, **kwargs):
-		pass
-
-	@staticmethod
-	def get_count(filters=None, **kwargs):
-		pass
-
-	@staticmethod
-	def get_stats(**kwargs):
-		pass
