b7f711485154997ef08478f0bad160358833afbd
Saqib Ansari <nextchamp.saqib@gmail.com>
2025-11-03 12:50:19 +0530

    fix: allow creation in install

--- frappe/core/doctype/permission_type/permission_type.py
+++ frappe/core/doctype/permission_type/permission_type.py
@@ -27,8 +27,23 @@ class PermissionType(Document):
 		label: DF.Data | None
 	# end: auto-generated types
 
+	def validate(self):
+		from frappe.permissions import rights
+
+		if self.name in rights:
+			frappe.throw(
+				_("Permission Type '{0}' is reserved. Please choose another name.").format(self.name)
+			)
+
+	def _can_write(self):
+		return (
+			frappe.conf.developer_mode
+			or frappe.flags.in_migrate
+			or frappe.flags.in_install
+		)
+
 	def on_update(self):
-		if not frappe.conf.developer_mode and not frappe.flags.in_migrate:
+		if not self._can_write():
 			frappe.throw(_("Creation of this document is only permitted in developer mode."))
 
 		from frappe.modules.export_file import export_to_files
@@ -59,7 +74,7 @@ class PermissionType(Document):
 			)
 
 	def on_trash(self):
-		if not frappe.conf.developer_mode and not frappe.flags.in_migrate:
+		if not self._can_write():
 			frappe.throw(_("Deletion of this document is only permitted in developer mode."))
 
 		for doctype in CUSTOM_FIELD_TARGET:
