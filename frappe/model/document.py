1ee7c6602c498daf1d918b9ac03171f6ae9ce0d6
Akhil Narang <me@akhilnarang.dev>
2025-12-10 16:52:37 +0530

    fix: update overloaded get_doc() signature
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -59,7 +59,9 @@ def get_doc(doctype: str, /) -> _SingleDocument:
 
 
 @overload
-def get_doc(doctype: str, name: str, /, *, for_update: bool | None = None) -> "Document":
+def get_doc(
+	doctype: str, name: str, /, *, for_update: bool | None = None, check_permission: str | bool | None = None
+) -> "Document":
 	"""Retrieve DocType from DB, doctype and name must be positional argument."""
 	pass
 
