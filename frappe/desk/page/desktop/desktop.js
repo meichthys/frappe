53fba7d7bd13f91b443cce19f464308f381eecb7
sokumon <sohamkulkarns9@gmail.com>
2026-01-13 19:31:26 +0530

    fix: show app icons instead of folder

--- frappe/desk/page/desktop/desktop.js
+++ frappe/desk/page/desktop/desktop.js
@@ -780,8 +780,7 @@ class DesktopIcon {
 	}
 
 	render_folder_thumbnail() {
-		let condition = this.icon_type == "App" && this.child_icons.length > 0;
-		if (this.icon_type == "Folder" || condition) {
+		if (this.icon_type == "Folder") {
 			if (!this.folder_wrapper) this.folder_wrapper = this.icon.find(".icon-container");
 			this.folder_wrapper.html("");
 			this.folder_grid = new DesktopIconGrid({
