15371f93b49207c55e604642a74d4982045be66f
Sagar Vora <16315650+sagarvora@users.noreply.github.com>
2026-01-16 21:53:41 +0530

    revert: "fix: ignore unsupported filter when querying dynamic link doctypes"
    This reverts commit bc590c084436eabb4aa19404dca554374a28664e.

--- frappe/contacts/address_and_contact.py
+++ frappe/contacts/address_and_contact.py
@@ -116,7 +116,6 @@ def filter_dynamic_link_doctypes(
 
 	txt = txt or ""
 	filters = filters or {}
-	filters.pop("name", None)  # ignore unsupported "name" filter - passed by validate_link_and_fetch
 
 	_doctypes_from_df = frappe.get_all(
 		"DocField",
