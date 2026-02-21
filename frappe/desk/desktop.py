d0e7ffb28533a66def4c4c59f6c5971260959342
KerollesFathy <kerollesfhabib@gmail.com>
2026-02-21 19:16:19 +0000

    fix: avoid append None to pages list

--- frappe/desk/desktop.py
+++ frappe/desk/desktop.py
@@ -490,7 +490,9 @@ def get_workspace_sidebar_items():
 		pages.extend(private_pages)
 
 	if len(pages) == 0:
-		pages.append(next((x for x in all_pages if x["title"] == "Welcome Workspace"), None))
+		welcome_workspace = next((x for x in all_pages if x["title"] == "Welcome Workspace"), None)
+		if welcome_workspace:
+			pages.append(welcome_workspace)
 
 	return {
 		"pages": pages,
