14aec471cbbecab6ff5a60402223310a459c9079
AarDG10 <aarol.dsouza@gmail.com>
2026-02-22 11:36:16 +0530

    refactor: better naming

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -1459,8 +1459,8 @@ def impersonate(user: str, reason: str):
 	notification.set("type", "Alert")
 	notification.insert(ignore_permissions=True)
 	# notify user via email too
-	email_exists = frappe.db.exists("Email Account", {"default_outgoing": 1, "awaiting_password": 0})
-	if email_exists:
+	outgoing_email_exists = frappe.db.exists("Email Account", {"default_outgoing": 1, "awaiting_password": 0})
+	if outgoing_email_exists:
 		user_email = frappe.db.get_value("User", user, "email")
 		email_message = _(
 			"User {0} has started an impersonation session as you. <br><br><b>Reason provided:</b> {1}"
