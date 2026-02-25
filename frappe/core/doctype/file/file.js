e30580385c41a84e9785cbd87f8829ad8e8673d6
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-02-25 15:47:13 +0530

    chore: run pre-commit

--- frappe/core/doctype/file/file.js
+++ frappe/core/doctype/file/file.js
@@ -46,9 +46,9 @@ frappe.ui.form.on("File", {
 
 		if (frm.doc.attached_to_name) {
 			const field = frm.get_field("attached_to_name");
-			field.$input_wrapper.find(".control-value").html(
-				`${frappe.utils.get_form_link(frm.doctype, frm.docname, true)}`
-			);
+			field.$input_wrapper
+				.find(".control-value")
+				.html(`${frappe.utils.get_form_link(frm.doctype, frm.docname, true)}`);
 		}
 	},
 
