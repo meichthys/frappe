14a923f94608af880f948ae066ead691c2dea892
Saqib Ansari <nextchamp.saqib@gmail.com>
2026-02-25 16:08:39 +0530

    fix: mis-aligned buttons in email dialog

--- frappe/public/js/frappe/views/communication.js
+++ frappe/public/js/frappe/views/communication.js
@@ -497,7 +497,11 @@ frappe.views.CommunicationComposer = class {
 			},
 		];
 
-		frappe.utils.add_select_group_button(clear_and_add_template, email_template_actions);
+		frappe.utils.add_select_group_button(
+			clear_and_add_template,
+			email_template_actions,
+			"btn-default"
+		);
 		$(fields.use_html.wrapper).addClass("mt-2 text-center").appendTo(clear_and_add_template);
 	}
 
