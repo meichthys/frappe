af11462900d9bfffa27c9d4a1b0ab0fabd6e9a7c
Aditya Patil <patiladitya781@gmail.com>
2026-02-25 15:51:41 +0530

    fix: spinner size and spacing to match frappe-ui

--- frappe/public/js/frappe/ui/dialog.js
+++ frappe/public/js/frappe/ui/dialog.js
@@ -199,7 +199,7 @@ frappe.ui.Dialog = class Dialog extends frappe.ui.FieldGroup {
 		this.has_primary_action = true;
 		var me = this;
 		const primary_btn = this.get_primary_btn().removeClass("hide").html(label);
-		const spinner = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" style="width: 16px; height: 16px; animation: spin 1s linear infinite;"><circle opacity=".25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/><path opacity=".25" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/></svg>`;
+		const spinner = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" style="width: 13px; height: 13px; animation: spin 1s linear infinite;"><circle opacity=".25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/><path opacity=".25" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/></svg>`;
 		if (typeof click == "function") {
 			primary_btn.off("click").on("click", function () {
 				me.primary_action_fulfilled = true;
@@ -219,7 +219,7 @@ frappe.ui.Dialog = class Dialog extends frappe.ui.FieldGroup {
 						.prop("disabled", true)
 						.addClass("btn-primary-dark")
 						.html(
-							`<div class="d-flex align-items-center justify-content-center" style="gap: 0.25rem;">
+							`<div class="d-flex align-items-center justify-content-center" style="gap: 0.45rem;">
 									${spinner}
 								${
 									loading_label
