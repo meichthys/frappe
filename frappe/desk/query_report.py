2dd8317bcef5523d696ceb15f0a1c7f88811dcb7
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-04-30 15:24:38 +0530

    refactor: extract non-labelable fieldtypes to constant

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -678,6 +678,9 @@ def get_xlsx_styles(metadata: XLSXMetadata, report_name: str | None = None) -> d
 	return styles
 
 
+NON_LABELABLE_FIELDTYPES = ("Currency", "Int", "Float", "Percent", "Date", "Datetime", "Time")
+
+
 def add_total_row(
 	result,
 	columns,
@@ -751,7 +754,7 @@ def add_total_row(
 	else:
 		first_col_fieldtype = columns[0].get("fieldtype")
 
-	if first_col_fieldtype not in ["Currency", "Int", "Float", "Percent", "Date", "Datetime", "Time"]:
+	if first_col_fieldtype not in NON_LABELABLE_FIELDTYPES:
 		total_row[0] = _("Total")
 
 	result.append(total_row)
